297dc652565572fb0dd41cfffcf489dd46866a21
phamtrung1411 <trungpv398@gmail.com>
2021-04-24 09:20:37 +0700

    update api

--- springboot_techwiz2/src/main/java/project_techwiz2/springboot_techwiz2/controller/api/ProductApiController.java
+++ springboot_techwiz2/src/main/java/project_techwiz2/springboot_techwiz2/controller/api/ProductApiController.java
@@ -2,9 +2,12 @@ package project_techwiz2.springboot_techwiz2.controller.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import project_techwiz2.springboot_techwiz2.model.core.Product;
+import project_techwiz2.springboot_techwiz2.model.dto.ProductDto;
 import project_techwiz2.springboot_techwiz2.repository.core.ProductRepository;
+import project_techwiz2.springboot_techwiz2.repository.dto.ProductDtoRepository;
 
 import java.util.List;
 
@@ -13,11 +16,25 @@ import java.util.List;
 public class ProductApiController {
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private ProductDtoRepository productDtoRepository;
 
     @RequestMapping(path = "")
-    public List<Product> getProduct()
+    public List<ProductDto> getProduct()
     {
-        List<Product> list = productRepository.findAll();
+        List<ProductDto> list = productDtoRepository.findAll();
         return list;
     }
+
+    @RequestMapping(path = "/category")
+    public List<ProductDto> getProductByCate(@RequestParam("cate_id")Integer cate_id)
+    {
+        List<ProductDto> list = productDtoRepository.getProByCate(cate_id);
+        return list;
+    }
+
+
+
+
+
 }
--- springboot_techwiz2/src/main/java/project_techwiz2/springboot_techwiz2/repository/dto/ProductDtoRepository.java
+++ springboot_techwiz2/src/main/java/project_techwiz2/springboot_techwiz2/repository/dto/ProductDtoRepository.java
@@ -1,12 +1,13 @@
-package project_techwiz2.springboot_techwiz2.repository.core;
+package project_techwiz2.springboot_techwiz2.repository.dto;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import project_techwiz2.springboot_techwiz2.model.core.Product;
+import project_techwiz2.springboot_techwiz2.model.dto.ProductDto;
 
 import java.util.List;
 
-public interface ProductRepository extends JpaRepository<Product,Integer> {
+public interface ProductDtoRepository extends JpaRepository<ProductDto,Integer> {
     @Query("SELECT p FROM Product p WHERE  cate_detail_id = ?1 AND status=1 ORDER BY priority ASC")
-    List<Product> getProByCate(int cate_id);
+    List<ProductDto> getProByCate(int cate_id);
 }
